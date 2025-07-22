1906626c43e7bddf5780203bed87f7698eabf407
Mohamed Elfaki <moh.omer24@gmail.com>
2025-07-22 14:37:43 +0300

    backward , forward and update params methods

--- main.py
+++ main.py
@@ -38,7 +38,46 @@ def softmax(Z):
         results.append(math.exp(j)/sum)
     return np.array(results)
 
+def ReLU_Derivative(Z):
+    return (Z>0).astype(float)
 
 
-def forward_prop(W1,b1,W2,b2):
+def forward_prop(W1,b1,W2,b2,X):
+    Z1 = W1 @ X + b1
+    A1 = ReLU(Z1)
+    Z2 =W2 @ A1 + b2
+    A2 = softmax(Z2)
+    return Z1, A1 ,Z2 ,A2
+
+
+def backward_prop(W1,b1,W2,b2,X2,X1,X,Y,Z1):
+    m = X.shape[1] 
+    dZ2 = X2 - Y
+    dW2 = 1/m * dZ2 @ X1.T
+
+    db2 = (1/m) * np.sum(dZ2)
+    dZ1 = W2.T @ dZ2 * ReLU_Derivative(Z1)
+    dW1 = (1/m) * dZ1 @ X.T
+    db1 = (1/m) * np.sum(dZ1)
+    return db1,dW1,db2,dW2
+
+
+def update_params(W1,b1,W2,b2,db1,dW1,db2,dW2,learningrate):
+    W1 += - learningrate*(dW1)
+    b1 += - learningrate*(db1)
+    W2 += -learningrate*(dW2)
+    b2 += - learningrate*(db2)
+
+    return W1,b1,W2,b2
+
+
+def training(X, Y, epochs, learningrate):
+   pass
+
+
+def main():
     pass
+
+main()
+
+  
